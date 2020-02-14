1ccc3697934083a09058782cbaca5e56093a93b1
Ryan Young <ryan@youngryan.com>
2020-02-14 11:55:56 -0800

    Map all stations at once in preparation for a better implementation.

--- autotable.py
+++ autotable.py
@@ -25,41 +25,53 @@ Trip = namedtuple('Trip', ['stops', 'path', 'consist',
 Stop = namedtuple('Stop', ['station', 'arrival', 'departure', 'commands'])
 
 
-def load_config(fp, install):
+def load_config(fp, install: MSTSInstall) -> Timetable:
     yd = yaml.safe_load(fp)
     route = next(r for r in install.routes if r.id.lower() == yd['route'].lower())
     tt = Timetable(route, yd['date'], yd['name'])
     for block in yd['gtfs']:
         feed_path = block['filename']
         feed = _load_gtfs(feed_path)
-        feed_stops = feed.get_stops()
-        for group in block['groups']:
+
+        # Select all filtered trips.
+        trips_sat = {}
+        for idx, group in enumerate(block['groups']):
             for _, row in _select_trips(
                     feed, yd['date'], group['selection']).iterrows():
-                stops = []
-                for stop_id, arrival, departure in _stops_and_times(
-                        feed, yd['date'], row['trip_id']):
-                    stop_name = feed_stops[feed_stops['stop_id'] == stop_id]\
-                        ['stop_name'].values[0]
-                    station = _map_station(stop_name, tt.route)
-                    if station is None:
-                        continue
-                    stops.append(Stop(station=station, arrival=arrival,
-                                      departure=departure, commands=''))
-                if len(stops) < 2:
-                    continue
-                tr = Trip(
-                    path=group['path'],
-                    consist=group['consist'],
-                    stops=stops,
-                    start_offset=group['start'],
-                    dispose_commands=group['dispose'])
-                tt.trips[(feed_path, row['trip_id'])] = tr
+                trip_id = row['trip_id']
+                if trip_id not in trips_sat:
+                    trips_sat[(idx, trip_id)] = _stops_and_times(
+                        feed, yd['date'], trip_id)
+
+        # Collect and map all station names.
+        feed_stops = feed.get_stops()
+        all_stops = set()
+        for _, stops in trips_sat.items():
+            all_stops.update(set(stop_id for stop_id, arrival, departure in stops))
+        station_map = _map_stations(route, feed, list(all_stops))
+
+        # Add all Trips to Timetable.
+        for (idx, trip_id), sat in trips_sat.items():
+            stops = [Stop(station=station_map[stop_id],
+                          arrival=arrival,
+                          departure=departure,
+                          commands='')
+                     for stop_id, arrival, departure in sat
+                     if station_map.get(stop_id, None) is not None]
+            if len(stops) < 2:
+                continue
+            group = block['groups'][idx]
+            tt.trips[(feed_path, trip_id)] = Trip(
+                path=group['path'],
+                consist=group['consist'],
+                stops=stops,
+                start_offset=group['start'],
+                dispose_commands=group['dispose'])
     return tt
 
 
 @lru_cache(maxsize=None)
-def _load_gtfs(path: Path):
+def _load_gtfs(path: Path) -> gk.feed.Feed:
     return gk.read_gtfs(path, dist_units='m') # Units don't matter (for now?).
 
 
@@ -74,7 +86,7 @@ def _select_trips(feed: gk.feed.Feed, date: dt.date, filters: dict):
     return sel
 
 
-def _stops_and_times(feed: gk.feed.Feed, date: dt.date, trip_id: str):
+def _stops_and_times(feed: gk.feed.Feed, date: dt.date, trip_id: str) -> list:
     stop_times = feed.get_stop_times(date=date.strftime('%Y%m%d'))
     trip = stop_times[stop_times['trip_id'] == trip_id]
     def parse_time(s):
@@ -89,15 +101,22 @@ def _stops_and_times(feed: gk.feed.Feed, date: dt.date, trip_id: str):
             for _, row in trip.sort_values(by='stop_sequence').iterrows()]
 
 
-@lru_cache(maxsize=None)
-def _map_station(station: str, route: Route):
-    matches = get_close_matches(station, route.stations())
-    n = len(matches)
-    if n == 0:
-        return None
-    elif n == 1:
-        return matches[0]
-    else:
-        raise KeyError(
-            f'ambiguous station: {station}\npotential candidates: {matches}')
+def _map_stations(route: Route, feed: gk.feed.Feed, stop_ids: list) -> dict:
+    feed_stops = feed.get_stops()
+    def stop_name(stop_id: str):
+        res = feed_stops[feed_stops['stop_id'] == stop_id]
+        return res['stop_name'].values[0]
+
+    station_map = {}
+    for stop_id in stop_ids:
+        matches = get_close_matches(stop_name(stop_id), route.stations())
+        n = len(matches)
+        if n == 0:
+            station_map[stop_id] = None
+        elif n == 1:
+            station_map[stop_id] = matches[0]
+        else:
+            raise KeyError(
+                f'ambiguous station: {station}\npotential candidates: {matches}')
+    return station_map
 
