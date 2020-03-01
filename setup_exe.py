0a58e33cffc8d0000995e6105e3d89c86b4ba36f
Ryan Young <ryan@youngryan.com>
2020-02-29 19:14:35 -0800

    Create standalone Windows executables with cx_Freeze.

---
+++ setup_exe.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = ['pyproj.datadir'],
+                    excludes = [],
+                    # https://github.com/anthony-tuininga/cx_Freeze/issues/278#issuecomment-542316877
+                    include_files = ['C:\\Windows\\System32\\vcruntime140.dll'])
+
+base = 'Console'
+
+executables = [
+    Executable('autotable.py', base=base)
+]
+
+setup(name='autotable',
+      version = '0.0.1',
+      description = 'An Open Rails timetable generator that uses GTFS data',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
